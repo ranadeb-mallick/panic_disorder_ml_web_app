34214a590a2d8c01468030d7fbb6561fb68bffb9
ranadeb-mallick <142046939+ranadeb-mallick@users.noreply.github.com>
2023-08-12 21:08:37 +0530

    Update Panic_Disorder.py

--- Panic_Disorder.py
+++ Panic_Disorder.py
@@ -18,7 +18,7 @@ background-color: rgba(0,0,0,0);
 }
 
 [data-testid="stSidebar"]{
-background-image: url("https://plus.unsplash.com/premium_photo-1682470006825-3d693cc05320?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=774&q=80");
+background-image: url("https://images.unsplash.com/photo-1691437195872-5e37ab3b3d60?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80");
 background-size: cover;
 }
 
